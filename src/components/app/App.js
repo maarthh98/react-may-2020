39fe61d1351658f261c8eb3b42e013055163d111
maarthh98 <maarthh98@gmail.com>
2020-06-02 22:58:43 +0200

    hw4

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -16,7 +16,8 @@ const sortingOptions = ['Sort By Default', 'Sort By Author'];
 class App extends Component {
   state = {
     posts: [...postsList],
-    // todo: 2) добавить под ключом selectedOption значение sortingOptions[0] (она будет хранить выбранную в данный моменит опцию)
+    selectedOption: sortingOptions[0]
+    // done: 2) добавить под ключом selectedOption значение sortingOptions[0] (она будет хранить выбранную в данный моменит опцию)
   };
 
   onSort = (selectedOption) => {
@@ -70,8 +71,8 @@ class App extends Component {
   };
 
   render() {
-    // todo 4) достать также в строке 92 из стейта selectedOption
-    const { posts } = this.state;
+    // done 4) достать также в строке 92 из стейта selectedOption
+    const { posts, selectedOption } = this.state;
 
     return (
       <div className="App">
@@ -92,10 +93,11 @@ class App extends Component {
             <button onClick={this.onSortByDefault}>By default</button>
 
             <DropDown
-
-
-
+            onSelect={this.onSort}
+            selectedOptions={selectedOption}
+            options={sortingOptions}
             />
+
           </div>
           <div className="d-flex posts-container">
             {
